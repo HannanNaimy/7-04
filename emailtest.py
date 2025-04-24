c2ea1ebd91c6f3f356c871102b9cb026b83caaa8
Hannan Naimy <hannannaimy@gmail.com>
2025-04-24 21:09:58 +0800

    Responsiveness Trial

--- emailtest.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-
-server = smtplib.SMTP("smtp.gmail.com", 587)  
-server.starttls()
-
-server.login("hannannaimy@gmail.com", "wjuy nxha nrlf knvw") 
-server.sendmail("hannannaimy@gmail.com", "1221106678@student.mmu.edu.my", "Test Email")
-server.quit()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re, random
-from flask import Flask, redirect, url_for, render_template, flash, session, request
+from flask import Flask, redirect, url_for, render_template, flash, session, request  
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User
 from flask_mail import Mail, Message
@@ -16,23 +16,30 @@ mail = Mail(app)
 with app.app_context():
     db.create_all()
 
+# Guest Page
+
 @app.route("/")
 def guest():
     return render_template("guest.html")
 
-#Register Page
+# Register Page
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST":
+    
+    # This checks if the user got here through submitting the form using "POST" method if not, 
+    # it will run |return render_template("register.html")|
+    
+    if request.method == "POST":            
         email = request.form.get("email")  
         username = request.form.get("username")
         password = request.form.get("password")
-
+        
+        # This checks if the email entered is mmu domain or not
         if not re.match(r"^[a-zA-Z0-9._%+-]+@[\w.]*mmu.edu.my$", email):
             flash("Email must be from MMU domain!", "error")
             return redirect(url_for("register"))
-        
+        # This checks if the email & username entered is already in the database or not
         existing_email = User.query.filter_by(email=email).first()
         existing_username = User.query.filter_by(username=username).first()
         
@@ -43,15 +50,14 @@ def register():
         if existing_username:
             flash("Username already taken!", "error")
             return redirect(url_for("register"))
-
-        # Adam's OTP start
+        # This generates a random 6 digit otp
         otp = str(random.randint(100000, 999999))
-
+        # This stores the following information in the session to be used at otp page later
         session["otp"] = otp
         session["temp_email"] = email
         session["temp_username"] = username
         session["temp_password"] = password
-
+        # This sends email containing otp code to the entered email 
         msg = Message("Your OTP Code", sender="ebtaskwebapp@outlook.com", recipients=[email])
         msg.body = f"Your OTP code is {otp}. Enter this to complete registration."
         mail.send(msg)
@@ -60,6 +66,8 @@ def register():
         return redirect(url_for("otp"))
 
     return render_template("register.html")
+
+# OTP Page
 
 @app.route("/OTP", methods=["GET", "POST"])
 def otp():
@@ -74,8 +82,7 @@ def otp():
             
             db.session.add(new_user)
             db.session.commit()
-
-            # Clear session after successful registration
+            # Removes following info from session
             session.pop("otp")
             session.pop("temp_email")
             session.pop("temp_username")
@@ -88,8 +95,8 @@ def otp():
             return redirect(url_for("otp"))
 
     return render_template("otp.html")
-        
-        # Adam's OTP end
+
+# Login Page
     
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -110,14 +117,9 @@ def login():
 
     return render_template("login.html")
 
-@app.route("/logout")
-def logout():
-    session.clear()
-    flash("You have been logged out.", "success")
-    return redirect(url_for("guest"))
+# Profile Page
 
-@app.route("/profile/<usr>", methods=["GET", "POST"])
-@app.route("/profile/<usr>", methods=["GET", "POST"])
+@app.route("/profile/<usr>", methods=["GET"])
 def profile(usr):
     if "user_id" not in session:
         flash("You must be logged in to view this page.", "error")
@@ -133,6 +135,14 @@ def profile(usr):
         return render_template("ownerprofile.html", usr=usr, email=user.email)
     else:
         return render_template("profile.html", usr=usr, email=user.email)
+
+  # Logout Function
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    flash("You have been logged out.", "success")
+    return redirect(url_for("guest"))  
 
 if __name__ == "__main__":
     app.run(debug=True)
