781a8142abf998b28b9dcdc35c725413864ec480
Adam Hazriq <adamhazriq8228@gmail.com>
2025-05-22 12:48:58 +0800

    Finish

--- config.py
+++ config.py
@@ -3,9 +3,15 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
     SECRET_KEY = os.urandom(11)
+
+    # Mail Configuration
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_USERNAME = "hannannaimy@gmail.com"
     MAIL_PASSWORD = "wjuy nxha nrlf knvw"
+
+    # Profile Picture Upload Configuration
+    UPLOAD_FOLDER = "static/profile_pictures/"
+    ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
